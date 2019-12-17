0d01eae6f1f533796fb7c0869eec2854c830f27d
Juri Wiens <juriwiens@gmail.com>
2019-12-17 22:09:50 +0100

    Export additional types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
 export { createServiceSkeleton, ServiceSkeletonOptions } from './skeleton'
+export { createPlugin, Plugin } from './plugin'
+export { Request, Response, RequestHandler, RouteOptions } from './route'
