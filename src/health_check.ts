0654b830c34203c3ba2d994c03ba42661cfd26c9
Juri Wiens <juriwiens@gmail.com>
2019-12-17 22:08:09 +0100

    Make health check endpoint configurable

--- src/health_check.ts
+++ src/health_check.ts
@@ -3,7 +3,7 @@ import { createPlugin } from './plugin'
 export default createPlugin<HealthCheckOptions>(async (app, opts) => {
   app.route({
     method: 'GET',
-    url: opts.url || '/admin/healthcheck',
+    url: opts?.url || '/admin/healthcheck',
     handler(request, reply) {
       reply.code(200).send()
     },
