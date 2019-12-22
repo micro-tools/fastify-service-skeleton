7a604e4e60e89d34e55a5f795323df825adff7fb
Juri Wiens <juriwiens@gmail.com>
2019-12-22 12:08:46 +0100

    Use keep-alive agents by default

--- src/http_client.ts
+++ src/http_client.ts
@@ -1,3 +1,5 @@
+import http from 'http'
+import https from 'https'
 import axios, { AxiosRequestConfig, AxiosInstance } from 'axios'
 import { Plugin } from './plugin'
 import fastifyPlugin = require('fastify-plugin')
@@ -5,6 +7,12 @@ import fastifyPlugin = require('fastify-plugin')
 export const httpClientPlugin: Plugin<HttpClientOptions> = fastifyPlugin(
   async (app, opts) => {
     const correlationIdHeader = opts.correlationIdHeader || 'Correlation-Id'
+    const axiosDefaults = {
+      // use keep-alive agents by default
+      httpAgent: new http.Agent({ keepAlive: true }),
+      httpsAgent: new https.Agent({ keepAlive: true }),
+      ...opts.axiosDefaults,
+    }
 
     app.decorateRequest('createHttpClient', null, ['correlationId'])
     app.addHook('onRequest', (request, reply, done) => {
