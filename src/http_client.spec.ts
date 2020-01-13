892c82b617518db6fddd85747d530b0d2b79dc2d
Juri Wiens <juriwiens@gmail.com>
2020-01-13 20:37:31 +0100

    Replace axios with got

--- src/http_client.spec.ts
+++ src/http_client.spec.ts
@@ -1,16 +1,17 @@
 import fastify from 'fastify'
 import uuidV4 from 'uuid/v4'
-import { httpClientPlugin } from './http_client'
-import { correlationIdPlugin } from './correlation_id/correlation_id'
+import nock from 'nock'
+import { httpClientPlugin, HttpClientPluginOptions } from './http_client'
+import { correlationIdPlugin } from './correlation_id'
 
 describe('httpClient plugin', () => {
-  it('allows to create a request specific httpClient', async () => {
+  it('allows to create a request-specific HTTP client', async () => {
     const app = await fastify()
       .register(correlationIdPlugin) // depends on correlationId
       .register(httpClientPlugin)
       .get('/', (request, reply) => {
         expect(typeof request.createHttpClient).toBe('function')
-        expect(typeof request.createHttpClient().request).toBe('function')
+        expect(typeof request.createHttpClient()).toBe('function')
         reply.code(200).send()
       })
       .ready()
@@ -19,24 +20,45 @@ describe('httpClient plugin', () => {
     await app.close()
   })
 
-  it('forwards the correlationId as header', async () => {
+  it('merges all headers and forwards the correlation-id header', async () => {
     const correlationId = uuidV4()
+    const fakeTargetUrl = 'https://test'
+    const checkOptionsHook = (opts: HttpClientPluginOptions) => {
+      expect(opts.headers).toMatchObject({
+        'correlation-id': correlationId,
+        'plugin-register-header': 'pluginRegister',
+        'create-client-header': 'createClient',
+        'request-header': 'request',
+      })
+    }
     const app = await fastify()
       .register(correlationIdPlugin) // depends on correlationId
-      .register(httpClientPlugin)
-      .get('/', (request, reply) => {
-        expect(
-          request.createHttpClient().defaults.headers['Correlation-Id'],
-        ).toBe(correlationId)
+      .register(httpClientPlugin, {
+        headers: { 'plugin-register-header': 'pluginRegister' },
+        hooks: { beforeRequest: [checkOptionsHook] },
+      })
+      .get('/', async (request, reply) => {
+        await request
+          .createHttpClient({
+            headers: { 'create-client-header': 'createClient' },
+          })
+          .get(fakeTargetUrl, { headers: { 'request-header': 'request' } })
         reply.code(200).send()
       })
       .ready()
+    const nockScope = nock(fakeTargetUrl)
+      .get('/')
+      .reply(200, '{"ok": true}')
+
     const res = await app.inject({
       method: 'GET',
       url: '/',
       headers: { 'Correlation-Id': correlationId },
     })
     expect(res.statusCode).toBe(200)
+    expect(nockScope.isDone())
+
+    nock.cleanAll()
     await app.close()
   })
 })
--- src/http_client.ts
+++ src/http_client.ts
@@ -1,42 +1,207 @@
-import http from 'http'
-import https from 'https'
-import axios, { AxiosRequestConfig, AxiosInstance } from 'axios'
+import fastifyPlugin from 'fastify-plugin'
+import * as promClient from 'prom-client'
+import got, {
+  Options,
+  Got,
+  Response as GotResponse,
+  NormalizedOptions,
+  GeneralError,
+} from 'got'
+import merge from 'lodash.merge'
 import { Plugin } from './plugin'
-import fastifyPlugin = require('fastify-plugin')
+import { FastifyRequest } from 'fastify'
+import { URL } from 'url'
+import { Logger } from './logging/logging.types'
 
-export const httpClientPlugin: Plugin<HttpClientOptions> = fastifyPlugin(
+export const httpClientPlugin: Plugin<HttpClientPluginOptions> = fastifyPlugin(
   async (app, opts) => {
-    const correlationIdHeader = opts.correlationIdHeader || 'Correlation-Id'
-    const axiosDefaults = {
-      // use keep-alive agents by default
-      httpAgent: new http.Agent({ keepAlive: true }),
-      httpsAgent: new https.Agent({ keepAlive: true }),
-      ...opts.axiosDefaults,
-    }
-
+    const correlationIdHeader = opts.correlationIdHeader || 'correlation-id'
     app.decorateRequest('createHttpClient', null, ['correlationId'])
     app.addHook('onRequest', (request, reply, done) => {
-      request.createHttpClient = config =>
-        axios.create({
-          ...opts.axiosDefaults,
-          ...config,
-          headers: { [correlationIdHeader]: request.correlationId },
-        })
+      request.createHttpClient = createRequestSpecificClientFactory(
+        correlationIdHeader,
+        request,
+      )
       done()
     })
   },
   { decorators: { request: ['correlationId'] } },
 )
 
-type AxiosFactory = (config?: AxiosRequestConfig) => AxiosInstance
+function createRequestSpecificClientFactory(
+  correlationIdHeader: string,
+  serverRequest: FastifyRequest,
+  instrumentation = new HttpClientInstrumentation(serverRequest.log as Logger),
+): HttpClientFactory {
+  return function createRequestSpecificClient(opts?: HttpClientOptions) {
+    return got.extend(
+      initClientOptions(
+        serverRequest.correlationId,
+        correlationIdHeader,
+        instrumentation,
+        opts,
+      ),
+    )
+  }
+}
 
-export interface HttpClientOptions {
-  axiosDefaults?: AxiosRequestConfig
+function initClientOptions(
+  correlationId: string,
+  correlationIdHeader: string,
+  instrumentation: HttpClientInstrumentation,
+  options?: HttpClientOptions,
+): HttpClientOptions {
+  const defaults: HttpClientOptions = {
+    responseType: 'json',
+    retry: { limit: 0 }, // do not retry by default
+  }
+  const enforcedOptions: HttpClientOptions = {
+    headers: { [correlationIdHeader]: correlationId },
+    hooks: {
+      beforeRequest: [
+        options => {
+          instrumentation.beforeRequest(options)
+        },
+      ],
+      afterResponse: [
+        response => {
+          instrumentation.receivedResponse(response)
+          return response
+        },
+      ],
+      beforeError: [
+        error => {
+          instrumentation.beforeError(error)
+          return error
+        },
+      ],
+      beforeRetry: [
+        (options, error, retryCount) =>
+          instrumentation.beforeRetry(options, error, retryCount),
+      ],
+      beforeRedirect: [
+        (options, response) => {
+          instrumentation.beforeRedirect(options, response)
+        },
+      ],
+    },
+  }
+  return merge(defaults, options, enforcedOptions)
+}
+
+export class HttpClientInstrumentation {
+  static requestDurations = new promClient.Histogram({
+    name: 'http_client_total_request_duration_seconds',
+    help: 'HTTP client total request durations.',
+    labelNames: ['host', 'status_code', 'is_retry', 'from_cache'],
+  })
+
+  constructor(private readonly logger: Logger) {}
+
+  beforeRequest(options: NormalizedOptions): void {
+    this.logger.debug(
+      { url: options.url, method: options.method },
+      'HTTP client starts request',
+    )
+  }
+
+  receivedResponse(clientResponse: HttpClientResponse): void {
+    this.logger.debug(
+      {
+        url: clientResponse.url,
+        requested_url:
+          clientResponse.requestUrl !== clientResponse.url
+            ? clientResponse.requestUrl
+            : undefined,
+        method: clientResponse.request.options.method,
+        status_code: clientResponse.statusCode,
+        status_message: clientResponse.statusMessage,
+        retry_count: clientResponse.retryCount,
+        from_cache: clientResponse.isFromCache,
+        http_version: clientResponse.httpVersion,
+        ip: clientResponse.ip,
+        durations: clientResponse.timings.phases,
+      },
+      'HTTP client has received response',
+    )
+    if (typeof clientResponse.timings.phases.total === 'number') {
+      HttpClientInstrumentation.requestDurations.observe(
+        {
+          host: new URL(clientResponse.url).host || 'unknown',
+          status_code: clientResponse.statusCode,
+          is_retry: Number(clientResponse.retryCount > 0),
+          from_cache:
+            typeof clientResponse.isFromCache === 'boolean'
+              ? Number(clientResponse.isFromCache)
+              : 'unknown',
+        },
+        clientResponse.timings.phases.total / 1000,
+      )
+    } else {
+      this.logger.error(
+        {
+          url: clientResponse.url,
+          requested_url:
+            clientResponse.requestUrl !== clientResponse.url
+              ? clientResponse.requestUrl
+              : undefined,
+          method: clientResponse.request.options.method,
+        },
+        'HTTP client request duration metric is missing',
+      )
+    }
+  }
+
+  beforeRetry(
+    options: NormalizedOptions,
+    error?: GeneralError,
+    retryCount?: number,
+  ): void {
+    const retryLimit = options.retry.limit
+    this.logger.debug(
+      {
+        url: options.url,
+        method: options.method,
+        retry_count: retryCount,
+        retry_limit: retryLimit,
+        error_name: error?.name,
+        error_stack: error?.stack,
+      },
+      `HTTP client will retry #${retryCount} of ${retryLimit}`,
+    )
+  }
+
+  beforeRedirect(
+    options: NormalizedOptions,
+    clientResponse: HttpClientResponse,
+  ) {
+    this.logger.debug(
+      { url: options.url, method: options.method },
+      'HTTP client request will be redirected',
+    )
+  }
+
+  beforeError(error: GeneralError) {
+    this.logger.error(
+      {
+        error_name: error.name,
+        error_stack: error.stack,
+      },
+      `HTTP client error: ${error.message}`,
+    )
+  }
+}
+
+export type HttpClient = Got
+export type HttpClientOptions = Options
+export type HttpClientResponse = GotResponse
+export type HttpClientFactory = (opts?: HttpClientOptions) => HttpClient
+export interface HttpClientPluginOptions extends HttpClientOptions {
   correlationIdHeader?: string
 }
 
 declare module 'fastify' {
   interface FastifyRequest {
-    createHttpClient: AxiosFactory
+    createHttpClient: HttpClientFactory
   }
 }
