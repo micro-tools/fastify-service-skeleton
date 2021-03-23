5797f573c249a34bc9e3f0c25935d66aaeedc756
Juri Wiens <juriwiens@gmail.com>
2021-03-23 22:16:07 +0100

    Enable eslint type checking

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,17 @@
-// eslint-disable-next-line no-undef
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"],
+  },
   plugins: ["@typescript-eslint"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
   ],
   rules: {
     "no-console": ["error"],
